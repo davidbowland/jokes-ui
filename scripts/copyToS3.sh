bd049d415f52604cfc6fb7dd7c9d38da8d5d79ff
David Bowland <dbowland1@gmail.com>
2021-09-15 00:10:11 -0500

    Update caching strategy

--- scripts/copyToS3.sh
+++ scripts/copyToS3.sh
@@ -7,7 +7,14 @@ if [[ -z "$1" ]]; then
   $(./scripts/assumeDeveloperRole.sh)
 fi
 
-# Deploy code by copying build output to S3
+### Deploy code by copying build output to S3
+
 cd public
-aws s3 sync . s3://jokes-ui-source/jokes-ui --delete --metadata-directive REPLACE --cache-control "max-age=31536000" --acl public-read --exclude "index.html"
-aws s3 sync . s3://jokes-ui-source/jokes-ui --metadata-directive REPLACE --cache-control "no-cache" --acl public-read --include "index.html"
+# Cache "forever" (one year)
+aws s3 sync . s3://jokes-ui-source/jokes-ui --exclude "*.html" --exclude "*.json" --exclude "*.xml" \
+  --metadata-directive REPLACE --cache-control "public, max-age=31536000, immutable" --acl public-read
+# Do not cache
+aws s3 sync . s3://jokes-ui-source/jokes-ui --include "*.html" --include "*.json" --include "*.xml" \
+  --metadata-directive REPLACE --cache-control "public, no-cache" --acl public-read
+# Cleanup unused files
+aws s3 sync . s3://jokes-ui-source/jokes-ui --delete
