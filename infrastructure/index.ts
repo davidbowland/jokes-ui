94e6f8ecee026aaca4360099057fcfe11048eb59
David Bowland <dbowland1@gmail.com>
2021-11-13 07:11:48 -0600

    Update infrastructure to output CDN URL

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -1,5 +1,11 @@
+import * as pulumi from '@pulumi/pulumi'
+
 // Import Pulumi configuration
 import './config'
 
 // Import modules to create resources
-import '@cloudfront'
+import { cdn } from '@cloudfront'
+
+// Output
+
+export const cdnUrl = pulumi.interpolate`https://${cdn.domainName}`
