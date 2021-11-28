9e4fc184dbda64aab973cd7c8adfc3e9421bfcbe
David Bowland <dbowland1@gmail.com>
2021-11-27 18:09:14 -0600

    Add jokes.bowland.link CDN alias

--- infrastructure/vars.ts
+++ infrastructure/vars.ts
@@ -5,4 +5,9 @@ export const createdFor = 'dbowland-jokes'
 
 /* CDN */
 
+export const acmCertificateArn = 'arn:aws:acm:us-east-1:494887012091:certificate/7f4606f7-302c-40a8-ac9d-e63db9b7a712'
 export const sourceS3Domain = 'jokes-ui-source.s3.us-east-2.amazonaws.com'
+
+/* Route 53 */
+
+export const hostedZoneId = 'Z01312547RGU1BYKIJXY'
