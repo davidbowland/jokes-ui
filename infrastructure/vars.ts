fbaa34ae4403e0f0b2a1e9cfa2d2ed0e97d3fd27
David Bowland <dbowland1@gmail.com>
2021-11-28 19:05:29 -0600

    Use us-east-1 cert for CDN

--- infrastructure/vars.ts
+++ infrastructure/vars.ts
@@ -5,7 +5,7 @@ export const createdFor = 'dbowland-jokes'
 
 /* CDN */
 
-export const acmCertificateArn = 'arn:aws:acm:us-east-2:494887012091:certificate/14a32175-0c26-4768-b71f-3fa611b8f5a2'
+export const acmCertificateArn = 'arn:aws:acm:us-east-1:494887012091:certificate/7f4606f7-302c-40a8-ac9d-e63db9b7a712'
 export const sourceS3Domain = 'jokes-ui-source.s3.us-east-2.amazonaws.com'
 
 /* Route 53 */
