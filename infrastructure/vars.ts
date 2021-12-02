e238230e5e19ba543a78753a7154d81ea86d6977
David Bowland <dbowland1@gmail.com>
2021-12-01 18:25:46 -0600

    Run npm audit fix

--- infrastructure/vars.ts
+++ infrastructure/vars.ts
@@ -1,11 +1,13 @@
 /* General */
 
+export const awsAccountId = process.env.AWS_ACCOUNT_ID
+
 export const createdBy = 'jokes-ui'
 export const createdFor = 'dbowland-jokes'
 
 /* CDN */
 
-export const acmCertificateArn = 'arn:aws:acm:us-east-1:494887012091:certificate/7f4606f7-302c-40a8-ac9d-e63db9b7a712'
+export const acmCertificateArn = `arn:aws:acm:us-east-1:${awsAccountId}:certificate/7f4606f7-302c-40a8-ac9d-e63db9b7a712`
 export const sourceS3Domain = 'jokes-ui-source.s3.us-east-2.amazonaws.com'
 
 /* Route 53 */
