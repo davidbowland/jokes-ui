901d3fb4f6861483d4b1e5be3b91a4fd82bdd73b
David Bowland <dbowland1@gmail.com>
2021-12-05 23:58:09 -0600

    Query zone ID

--- infrastructure/vars.ts
+++ infrastructure/vars.ts
@@ -1,6 +1,7 @@
 /* General */
 
 export const awsAccountId = process.env.AWS_ACCOUNT_ID
+export const domainName = 'bowland.link'
 
 export const createdBy = 'jokes-ui'
 export const createdFor = 'jokes'
@@ -9,7 +10,3 @@ export const createdFor = 'jokes'
 
 export const acmCertificateArn = `arn:aws:acm:us-east-1:${awsAccountId}:certificate/7f4606f7-302c-40a8-ac9d-e63db9b7a712`
 export const sourceS3Domain = 'jokes-ui-source.s3.us-east-2.amazonaws.com'
-
-/* Route 53 */
-
-export const hostedZoneId = 'Z01312547RGU1BYKIJXY'
