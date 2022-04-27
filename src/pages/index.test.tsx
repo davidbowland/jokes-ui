9327f6548f4f5a47ef6cab0fcefdb2cf580ea407
David Bowland <dbowland1@gmail.com>
2022-04-27 00:13:04 -0500

    Stop mocking wrapPageElements elements

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -3,21 +3,15 @@ import React from 'react'
 import { mocked } from 'jest-mock'
 import { render } from '@testing-library/react'
 
-import Authenticated from '@components/auth'
 import Humor from './index'
 import Joke from '@components/joke'
-import Themed from '@components/themed'
 
 jest.mock('@aws-amplify/analytics')
-jest.mock('@components/auth')
 jest.mock('@components/joke')
-jest.mock('@components/themed')
 
 describe('Humor page', () => {
   beforeAll(() => {
-    mocked(Authenticated).mockImplementation(({ children }) => <>{children}</>)
     mocked(Joke).mockReturnValue(<></>)
-    mocked(Themed).mockImplementation(({ children }) => <>{children}</>)
   })
 
   test('expect rendering Humor renders Joke', () => {
