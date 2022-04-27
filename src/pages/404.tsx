7a08af32baf278bbee99714bda17bf00f75b8ce9
David Bowland <dbowland1@gmail.com>
2022-04-26 22:49:10 -0500

    Utilize wrapPageElement

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,20 +1,14 @@
-import Paper from '@mui/material/Paper'
 import React from 'react'
 
 import '@config/amplify'
 import ServerErrorMessage from '@components/server-error-message'
-import Themed from '@components/themed'
 
 const NotFound = (): JSX.Element => {
   return (
-    <Themed>
-      <Paper elevation={3} sx={{ margin: '1em auto', maxWidth: '900px' }}>
-        <ServerErrorMessage title="404: Not Found">
-          The resource you requested is unavailable. If you feel you have reached this page in error, please contact the
-          webmaster.
-        </ServerErrorMessage>
-      </Paper>
-    </Themed>
+    <ServerErrorMessage title="404: Not Found">
+      The resource you requested is unavailable. If you feel you have reached this page in error, please contact the
+      webmaster.
+    </ServerErrorMessage>
   )
 }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,18 @@
 import { Helmet } from 'react-helmet'
-import Paper from '@mui/material/Paper'
 import React from 'react'
 
-import Authenticated from '@components/auth'
 import Joke from '@components/joke'
-import Themed from '@components/themed'
 
 const Humor = (): JSX.Element => {
   return (
-    <Themed>
+    <>
       <Helmet>
         <title>Humor | dbowland.com</title>
       </Helmet>
-      <Paper elevation={3} sx={{ margin: 'auto', maxWidth: '900px' }}>
-        <Authenticated>
-          <main className="main-content">
-            <Joke initialize={typeof window !== 'undefined'} />
-          </main>
-        </Authenticated>
-      </Paper>
-    </Themed>
+      <main className="main-content">
+        <Joke initialize={typeof window !== 'undefined'} />
+      </main>
+    </>
   )
 }
 
