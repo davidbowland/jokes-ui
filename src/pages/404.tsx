a88df7b66bfbd313992ad3909e880a822ec4dd72
David Bowland <dbowland1@gmail.com>
2023-05-14 21:05:26 -0500

    Fix 404 when loading specific joke

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,12 +3,16 @@ import React from 'react'
 import ServerErrorMessage from '@components/server-error-message'
 
 const NotFound = (): JSX.Element => {
-  return (
-    <ServerErrorMessage title="404: Not Found">
-      The resource you requested is unavailable. If you feel you have reached this page in error, please contact the
-      webmaster.
-    </ServerErrorMessage>
-  )
+  const display404 = typeof window !== 'undefined' && window.location.pathname.match(/^\/j\/[^/]+$/) === null
+  if (display404) {
+    return (
+      <ServerErrorMessage title="404: Not Found">
+        The resource you requested is unavailable. If you feel you have reached this page in error, please contact the
+        webmaster.
+      </ServerErrorMessage>
+    )
+  }
+  return <></>
 }
 
 export default NotFound
